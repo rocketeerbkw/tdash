d87b255f35c9b1cce9e7f47b38992cd4546f3a5a
Brandon Williams <rocketeerbkw@gmail.com>
2014-01-21 23:20:22 -0600

    Remove winstone binary. Move related files.

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,3 +11,5 @@ unzip yuicompressor-2.4.2.zip
 cp yuicompressor-2.4.2/build/yuicompressor-2.4.2.jar /vagrant/bin/
 wget http://repo.typesafe.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-launch/0.10.0/sbt-launch.jar
 cp sbt-launch.jar /vagrant/bin/sbt-launch_0.10.jar
+wget http://downloads.sourceforge.net/project/winstone/winstone/v0.9.10/winstone-0.9.10.jar
+cp winstone-0.9.10.jar /vagrant/bin/
