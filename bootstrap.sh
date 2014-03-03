c3d249da82162c6e839644a965846450489f7568
Brandon Williams <rocketeerbkw@gmail.com>
2014-03-02 23:40:24 -0600

    Install postgresql, create database, load schema

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 # System deps
 apt-get update
-apt-get install -y default-jre unzip git
+apt-get install -y default-jre unzip git postgresql
 
 # Program deps
 mkdir /vagrant/bin
@@ -13,3 +13,20 @@ wget http://repo.typesafe.com/typesafe/ivy-releases/org.scala-tools.sbt/sbt-laun
 cp sbt-launch.jar /vagrant/bin/sbt-launch_0.10.jar
 wget http://downloads.sourceforge.net/project/winstone/winstone/v0.9.10/winstone-0.9.10.jar
 cp winstone-0.9.10.jar /vagrant/bin/
+
+# Create database
+sudo -u postgres -H sh -c "createdb tdash"
+sudo -u postgres -H sh -c "echo \"ALTER ROLE postgres WITH PASSWORD 'xyz'\" | psql tdash"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/tdashDB.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion2.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion3.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion4.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion5.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion6.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion7.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion8.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion9.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion10.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion11.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion12.sql"
+sudo -u postgres -H sh -c "psql tdash < /vagrant/admin/db/updateToVersion13.sql"
