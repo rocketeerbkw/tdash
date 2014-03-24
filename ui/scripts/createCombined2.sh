36297105753bc1b94462567cd076e82e75363b97
Brandon Williams <rocketeerbkw@gmail.com>
2014-03-23 20:04:20 -0500

    Fix debug branches in makefiles

--- ui/scripts/createCombined2.sh
+++ ui/scripts/createCombined2.sh
@@ -4,16 +4,16 @@ fi
 if [ -z "$BIN_DIR" ]; then
   BIN_DIR=../../bin;
 fi
-if [ -z "$COMPRESSOR" ]; then
-  COMPRESSOR="java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar";
+if [ -z "$COMPRESS_CMD" ]; then
+  COMPRESS_CMD="java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar";
 fi
 
-echo $COMPRESSOR
+echo ${COMPRESS_CMD}
 
-$COMPRESSOR ${SRC_DIR}jquery.scrollTo-min.js > ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}jquery.ba-longurl.min.js >> ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}bitly.js >> ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}oauth_july2010.js >> ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}sha1.js >> ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}persist-new.js >> ${SRC_DIR}compound2.js
-$COMPRESSOR ${SRC_DIR}jquery.simplemodal-1.3.3.min.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}jquery.scrollTo-min.js > ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}jquery.ba-longurl.min.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}bitly.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}oauth_july2010.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}sha1.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}persist-new.js >> ${SRC_DIR}compound2.js
+${COMPRESS_CMD} ${SRC_DIR}jquery.simplemodal-1.3.3.min.js >> ${SRC_DIR}compound2.js
--- ui/scripts/createIndexCombined.sh
+++ ui/scripts/createIndexCombined.sh
@@ -4,10 +4,12 @@ fi
 if [ -z "$BIN_DIR" ]; then
   BIN_DIR=../../bin;
 fi
+if [ -z "$COMPRESS_CMD" ]; then
+  COMPRESS_CMD="java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar";
+fi
 
-set -x
-set -e
+echo ${COMPRESS_CMD}
 
-java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar ${SRC_DIR}oauth_july2010.js > ${SRC_DIR}indexComp2.js
-java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar ${SRC_DIR}sha1.js >> ${SRC_DIR}indexComp2.js
-java -jar ${BIN_DIR}/yuicompressor-2.4.2.jar ${SRC_DIR}index2.js >> ${SRC_DIR}indexComp2.js
+${COMPRESS_CMD} ${SRC_DIR}oauth_july2010.js > ${SRC_DIR}indexComp2.js
+${COMPRESS_CMD} ${SRC_DIR}sha1.js >> ${SRC_DIR}indexComp2.js
+${COMPRESS_CMD} ${SRC_DIR}index2.js >> ${SRC_DIR}indexComp2.js
