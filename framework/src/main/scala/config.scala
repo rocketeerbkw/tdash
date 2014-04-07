7e35b76b924e804140137e5f93bc271455a55bce
Brandon Williams <rocketeerbkw@gmail.com>
2014-04-07 10:01:29 -0500

    Fix config.scala to return strings for some settings

--- framework/src/main/scala/config.scala
+++ framework/src/main/scala/config.scala
@@ -1,8 +1,8 @@
 package bhoot
 
 object Config {
-  val twitterDesktopOauthKey = UtilsServlet.initParms.get("oauthKey")
-  val twitterDesktopOauthSecret = UtilsServlet.initParms.get("oauthSecret")
+  val twitterDesktopOauthKey = UtilsServlet.initParms.get("oauthKey").get
+  val twitterDesktopOauthSecret = UtilsServlet.initParms.get("oauthSecret").get
   val domainName = UtilsServlet.initParms.get("domainName").getOrElse("127.0.0.1")
   val port = UtilsServlet.initParms.get("port").getOrElse("8080")
   val baseUrl = "http://" + domainName + ":" + port + "/"
