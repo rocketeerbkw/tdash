e0372ad2bf3095cd0399ccfbe1732957bca09764
Brandon Williams <rocketeerbkw@gmail.com>
2014-06-20 23:02:50 -0500

    Fix postgresql function to accept bigint for large twitter ids

---
+++ admin/db/updateToVersion15.sql
@@ -0,0 +1,34 @@
+SELECT version from meta_info;
+\prompt "Did you see 14 above? If no, press Ctrl+c now!" correctVersion
+
+DROP FUNCTION set_old_on_top(INTEGER, BOOLEAN);
+
+CREATE LANGUAGE plpgsql;
+
+CREATE FUNCTION set_old_on_top(user_id_a BIGINT, setting BOOLEAN) RETURNS VOID AS
+$$
+  BEGIN
+    LOOP
+      -- first try to update the key
+      UPDATE user_settings SET old_on_top = setting WHERE user_id = user_id_a;
+
+      IF found THEN
+        RETURN;
+      END IF;
+
+      -- not there, so try to insert the key
+      -- if someone else inserts the same key concurrently,
+      -- we could get a unique-key failure
+      BEGIN
+        INSERT INTO user_settings(user_id,old_on_top) VALUES (user_id_a, setting);
+        RETURN;
+      EXCEPTION WHEN unique_violation THEN
+        -- do nothing, and loop to try the UPDATE again
+      END;
+    END LOOP;
+  END;
+$$
+LANGUAGE plpgsql;
+
+UPDATE meta_info
+  SET version = 15;
